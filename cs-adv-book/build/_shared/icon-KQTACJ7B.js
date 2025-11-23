98674b259fd35ca0e1fd4e49a50a600904ce6670
Leszek Tarkowski <leszek.tarkowski@gmail.com>
2025-11-23 13:07:06 +0100

    fix base url

--- cs-adv-book/build/_shared/icon-KQTACJ7B.js
+++ cs-adv-book/build/_shared/icon-KQTACJ7B.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-VDAF25SX.js";import"/build/_shared/chunk-2NH4LW52.js";export default a();
+import{a}from"/cs-adv-book/build/_shared/chunk-VDAF25SX.js";import"/cs-adv-book/build/_shared/chunk-2NH4LW52.js";export default a();
